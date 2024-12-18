e46e43a720916d730e086eacf48d4a3ab9fb73ce
RamX23 <112005895+RamX23@users.noreply.github.com>
2024-12-18 15:07:08 +0530

    Update constants.js

--- src/redux/api/constants.js
+++ src/redux/api/constants.js
@@ -1,8 +1,8 @@
-export const BASE_URL='https://e-commerce-frontend1-t8fy.onrender.com/'
-export const USER_URL='https://e-commerce-frontend1-t8fy.onrender.com//api/users'
-export const CATEGORY_URL='https://e-commerce-frontend1-t8fy.onrender.com/api/category'
-export const PRODUCT_URL='https://e-commerce-frontend1-t8fy.onrender.com/api/products'
-export const UPLOAD_URL='https://e-commerce-frontend1-t8fy.onrender.com/api/upload'
+export const BASE_URL='https://e-commerce-finalproject1.onrender.com/'
+export const USER_URL='https://e-commerce-finalproject1.onrender.com/api/users'
+export const CATEGORY_URL='https://e-commerce-finalproject1.onrender.com/api/category'
+export const PRODUCT_URL='https://e-commerce-finalproject1.onrender.com/api/products'
+export const UPLOAD_URL='https://e-commerce-finalproject1.onrender.com/api/upload'
 
-export const ORDERS_URL = "https://e-commerce-frontend1-t8fy.onrender.com/api/orders";
-export const PAYPAL_URL = "https://e-commerce-frontend1-t8fy.onrender.com/api/config/paypal";
+export const ORDERS_URL = "https://e-commerce-finalproject1.onrender.com/api/orders";
+export const PAYPAL_URL = "https://e-commerce-finalproject1.onrender.com/api/config/paypal";
