f74d4fac36fa53c6d475abb37db6b9ed61508720
RamX23 <112005895+RamX23@users.noreply.github.com>
2024-12-18 16:57:39 +0530

    Update CategoryApiSlice.js

--- src/redux/CategoryApiSlice.js
+++ src/redux/CategoryApiSlice.js
@@ -1,42 +1,47 @@
+import { deleteCategory, updateCategory } from "../../../backend/controllers/categoryController";
 import { apiSlice } from "./api/apiSlice";
 import { CATEGORY_URL } from "./api/constants";
 
-export const categoryApiSlice=apiSlice.injectEndpoints({
-    endpoints:(builder)=>({
-        createCategory:builder.mutation({
-            query:(newCategory)=>({
-                url:`${CATEGORY_URL}`,
-                method:'POST',
-                body:newCategory,
-            })
-        }),
-
-        updateCategory:builder.mutation({
-            query:({categoryId,updatedCategory})=>({
-                url:`${CATEGORY_URL}/${categoryId}`,
-                method:'PUT',
-                body:updatedCategory,
-            })
-        }),
-
-        deleteCategory:builder.mutation({
-            query:({categoryId})=>({
-                url:`${CATEGORY_URL}/${categoryId},`,
-                method:'DELETE',
-            })
-        }),
-
-        fetchCategories:builder.query({
-            query:()=>({
-                url:`${CATEGORY_URL}/categories`,
-            })
-        }),
+export const categoryApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    createCategory: builder.mutation({
+      query: (newCategory) => ({
+        url: `${CATEGORY_URL}`,
+        method: "POST",
+        body: newCategory,
+        credentials: "include",
+      }),
     }),
+
+    updateCategory: builder.mutation({
+      query: ({ categoryId, updatedCategory }) => ({
+        url: `${CATEGORY_URL}/${categoryId}`,
+        method: "PUT",
+        body: updatedCategory,
+        credentials: "include",
+      }),
+    }),
+
+    deleteCategory: builder.mutation({
+      query: ({ categoryId }) => ({
+        url: `${CATEGORY_URL}/${categoryId}`,
+        method: "DELETE",
+        credentials: "include",
+      }),
+    }),
+
+    fetchCategories: builder.query({
+      query: () => ({
+        url: `${CATEGORY_URL}/categories`,
+        credentials: "include",
+      }),
+    }),
+  }),
 });
 
 export const {
-useCreateCategoryMutation,
-useDeleteCategoryMutation,
-useUpdateCategoryMutation,
-useFetchCategoriesQuery,
-} = categoryApiSlice
+  useCreateCategoryMutation,
+  useDeleteCategoryMutation,
+  useUpdateCategoryMutation,
+  useFetchCategoriesQuery,
+} = categoryApiSlice;
