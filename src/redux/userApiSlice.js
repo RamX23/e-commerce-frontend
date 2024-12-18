68dc458ce296adca45a12069ab8bc55f75ed21b4
RamX23 <112005895+RamX23@users.noreply.github.com>
2024-12-18 16:58:06 +0530

    Update userApiSlice.js

--- src/redux/userApiSlice.js
+++ src/redux/userApiSlice.js
@@ -1,64 +1,80 @@
 import { apiSlice } from "./api/apiSlice";
 import { USER_URL } from "./api/constants";
 
-export const userApiSlice=apiSlice.injectEndpoints({
-    endpoints:(builder)=>({
-        login:builder.mutation({
-            query:(data)=>({
-                url:`${USER_URL}/auth`,
-                method:"POST",
-                body:data,
-            }),
-        }),
-        logout:builder.mutation({
-         query: ()=>({
-            url:`${USER_URL}/logout`,
-            method:"POST",
-         }),
-        }),
-        register:builder.mutation({
-         query:(data)=>({
-            url:`${USER_URL}/register`,
-            method:"POST",
-            body:data,
-         }),
-        }),
-        profile:builder.mutation({
-            query:(data)=>({
-                url:`${USER_URL}/profile`,
-                method:"PUT",
-                body:data,
-            }),
-        }),
-      getUsers:builder.query({
-            query:()=>({
-                url:`${USER_URL}`,
-            }),
-            providesTags:['User'],
-            keepUnusedDataFor:5,
-        }),
-
-        deleteUser:builder.mutation({
-            query:userId=>({
-                url:`${USER_URL}/${userId}`,
-                method:"DELETE",
-            }),
-        }),
-        getUserDetails:builder.query({
-            query:(id)=>({
-                url:`${USER_URL}/${id}`,
-            }),
-            keepUnusedDataFor:5,
-        }),
-        updateUser:builder.mutation({
-            query:data=>({
-            url:`${USER_URL}/${data.userId}`,
-            method:"PUT",
-            body:data,
-            }),
-            invalidatesTags :["User"]
-        })
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/auth`,
+        method: "POST",
+        body: data,
+        credentials: "include",
+      }),
     }),
-})
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USER_URL}/logout`,
+        method: "POST",
+        credentials: "include",
+      }),
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/register`,
+        method: "POST",
+        body: data,
+        credentials: "include",
+      }),
+    }),
+    profile: builder.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/profile`,
+        method: "PUT",
+        body: data,
+        credentials: "include",
+      }),
+    }),
+    getUsers: builder.query({
+      query: () => ({
+        url: `${USER_URL}`,
+        credentials: "include",
+      }),
+      providesTags: ["User"],
+      keepUnusedDataFor: 5,
+    }),
+    deleteUser: builder.mutation({
+      query: (userId) => ({
+        url: `${USER_URL}/${userId}`,
+        method: "DELETE",
+        credentials: "include",
+      }),
+    }),
+    getUserDetails: builder.query({
+      query: (id) => ({
+        url: `${USER_URL}/${id}`,
+        credentials: "include",
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/${data.userId}`,
+        method: "PUT",
+        body: data,
+        credentials: "include",
+      }),
+      invalidatesTags: ["User"],
+    }),
+  }),
+});
 
-export const {useLoginMutation,useLogoutMutation,useRegisterMutation,useProfileMutation,useGetUsersQuery,useDeleteUserMutation,useGetUserDetailsQuery,useUpdateUserMutation} = userApiSlice; 
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useProfileMutation,
+  useGetUsersQuery,
+  useDeleteUserMutation,
+  useGetUserDetailsQuery,
+  useUpdateUserMutation,
+} = userApiSlice;
